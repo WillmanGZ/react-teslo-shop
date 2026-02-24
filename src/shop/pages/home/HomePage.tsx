2a9a211f8044757ab0670c3b4250ef9aa21bae1c
WillmanGZ <willmang@uninorte.edu.co>
2026-02-24 14:49:04 -0500

    Add product card, products grid, and filter sidebar components

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,10 +1,13 @@
 import { CustomPagination } from "@/components/custom/CustomPagination";
+import { products } from "@/mocks/products.mock";
 import CustomJumbotron from "@/shop/components/CustomJumbotron";
+import ProductsGrid from "@/shop/components/ProductsGrid";
 
 export const HomePage = () => {
   return (
     <>
       <CustomJumbotron title="Todos los productos"></CustomJumbotron>
+      <ProductsGrid products={products}></ProductsGrid>
       <CustomPagination totalPages={7}></CustomPagination>
     </>
   );
