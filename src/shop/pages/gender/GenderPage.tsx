ff4a8ecdf098c3eb525b265b07979a733e9291f1
WillmanGZ <willmang@uninorte.edu.co>
2026-02-25 16:52:02 -0500

    feat: enhance registration page with form and social login options
    - Implemented a new registration page with a form for full name, email, and password.
    - Added buttons for social login (Apple, Google, Meta).
    - Included a logo component and terms and conditions links.
    
    fix: update button styles for better user interaction
    
    - Added cursor pointer to button variants for improved UX.
    
    feat: integrate custom logo into footer and header components
    
    - Replaced text branding with a custom logo in the footer.
    - Updated header to include the custom logo and adjusted navigation links.
    
    feat: enhance gender page with jumbotron and product grid
    
    - Added a jumbotron component to display gender-specific product titles.
    - Integrated a product grid and pagination for better product display.
    
    chore: add placeholder images and logos for UI consistency
    
    - Added placeholder images for user profiles and logos in various formats.
    
    feat: create a reusable custom logo component
    
    - Developed a CustomLogo component for consistent branding across the application.

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,3 +1,21 @@
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { products } from "@/mocks/products.mock";
+import CustomJumbotron from "@/shop/components/CustomJumbotron";
+import ProductsGrid from "@/shop/components/ProductsGrid";
+import { useParams } from "react-router";
+
 export const GenderPage = () => {
-  return <div>GenderPage</div>;
+  const { gender } = useParams();
+  const genderLabel =
+    gender === "men" ? "Hombres" : gender === "women" ? "Mujeres" : "Niños";
+
+  return (
+    <>
+      <CustomJumbotron
+        title={`Productos para ${genderLabel}`}
+      ></CustomJumbotron>
+      <ProductsGrid products={products}></ProductsGrid>
+      <CustomPagination totalPages={7}></CustomPagination>
+    </>
+  );
 };
