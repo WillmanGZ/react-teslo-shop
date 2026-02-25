e2c5b53a73953e2ec45a80df76623257da8e41ea
WillmanGZ <willmang@uninorte.edu.co>
2026-02-25 14:35:32 -0500

    feat: add filter functionality

--- src/shop/components/ProductsGrid.tsx
+++ src/shop/components/ProductsGrid.tsx
@@ -3,12 +3,25 @@ import { type Product } from "@/mocks/products.mock";
 import { Filter, Grid, List } from "lucide-react";
 import ProductCard from "./ProductCard";
 import FilterSidebar from "./FilterSidebar";
+import { useSearchParams } from "react-router";
+import { useState } from "react";
 
 interface Props {
   products: Product[];
 }
 
 const ProductsGrid = ({ products }: Props) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [showFilters, setShowFilters] = useState(false);
+
+  const viewMode = searchParams.get("viewMode") || "grid";
+
+  const handlViewModeChange = (mode: "grid" | "list") => {
+    searchParams.set("viewMode", mode);
+    setSearchParams(searchParams);
+  };
+
   return (
     <section className="py-12 px-4 lg:px-8">
       <div className="container mx-auto">
@@ -24,8 +37,8 @@ const ProductsGrid = ({ products }: Props) => {
             <Button
               variant="outline"
               size="sm"
-              /*               onClick={() => setShowFilters(!showFilters)}
-               */ className="lg:hidden"
+              onClick={() => setShowFilters(!showFilters)}
+              className="lg:hidden"
             >
               <Filter className="h-4 w-4 mr-2" />
               Filtros
@@ -33,18 +46,18 @@ const ProductsGrid = ({ products }: Props) => {
 
             <div className="hidden md:flex border rounded-md">
               <Button
-                /*                 variant={viewMode === "grid" ? "default" : "ghost"}
-                 */ size="sm"
-                /*                 onClick={() => setViewMode("grid")}
-                 */ className="rounded-r-none"
+                variant={viewMode === "grid" ? "default" : "ghost"}
+                size="sm"
+                onClick={() => handlViewModeChange("grid")}
+                className="rounded-r-none"
               >
                 <Grid className="h-4 w-4" />
               </Button>
               <Button
-                /*                 variant={viewMode === "list" ? "default" : "ghost"}
-                 */ size="sm"
-                /*                 onClick={() => setViewMode("list")}
-                 */ className="rounded-l-none"
+                variant={viewMode === "list" ? "default" : "ghost"}
+                size="sm"
+                onClick={() => handlViewModeChange("list")}
+                className="rounded-l-none"
               >
                 <List className="h-4 w-4" />
               </Button>
@@ -59,7 +72,7 @@ const ProductsGrid = ({ products }: Props) => {
           </div>
 
           {/* Mobile Filters */}
-          {/*           {showFilters && (
+          {showFilters && (
             <div className="fixed inset-0 z-50 bg-background p-4 lg:hidden">
               <div className="flex items-center justify-between mb-6">
                 <h3 className="text-lg font-semibold">Filtros</h3>
@@ -73,16 +86,16 @@ const ProductsGrid = ({ products }: Props) => {
               </div>
               <FilterSidebar />
             </div>
-          )} */}
+          )}
 
           {/* Products Grid */}
           <div className="flex-1">
             <div
-            /*               className={
+              className={
                 viewMode === "grid"
                   ? "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
                   : "space-y-4"
-              } */
+              }
             >
               {products.map((product) => (
                 <ProductCard
